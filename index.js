701bf15b3d17aa7cacacd89905b772f948f4d25d
mercury landed <wsk3201@gmail.com>
2013-10-25 11:05:55 +0800

    modify

--- index.js
+++ index.js
@@ -5,31 +5,6 @@ var sourceDir = hexo.source_dir;
 var htmlTag = hexo.util.html_tag;
 var route = hexo.route;
 
-hexo.extend.generator.register(function(locals, render, callback){
-  //check images folder exists
-  var srcdir = sourceDir + '_images' + path.sep;
-  var tardir = publicDir + 'images' + path.sep;
-  fs.exists(srcdir,function(exists){
-    if (exists) {
-      fs.readdir(srcdir,function(err,files){
-        files.forEach(function(file){
-          var cb = function(fn){
-            if (!fs.existsSync(tardir)){
-              fs.mkdirSync(tardir);
-            }
-            fn(null,fs.createReadStream(srcdir + file));
-          };
-          cb.modified = true;
-          route.set('images' + path.sep + file,cb);
-        });
-        callback();
-      });
-    } else {
-      callback();
-    }
-  });
-});
-
 var localImage = function(args,content){
   var imageName = args[0]; //images name
   var classes = args[1] || "";
